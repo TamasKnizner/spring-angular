bea17e1cb1fdb7878755a8c3f4691455d200bf92
Knizner Tamás <Knizner Tams>
2017-02-18 18:44:01 +0100

    filename moved to properties file

--- src/main/java/hu/kniznertamas/contentreader/service/UserReaderService.java
+++ src/main/java/hu/kniznertamas/contentreader/service/UserReaderService.java
@@ -3,19 +3,25 @@ package hu.kniznertamas.contentreader.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import hu.kniznertamas.contentreader.domain.User;
 import hu.kniznertamas.contentreader.service.reader.CSVReader;
 
 @Service
+@PropertySource("classpath:application.properties")
 public class UserReaderService {
+
+    @Value("${data.user.filename}")
+    private String fileName;
 
     @Autowired
     private CSVReader<User> userReader;
 
     public List<User> readUsers() {
-        return userReader.read("static/data.csv");
+        return userReader.read(fileName);
     }
 
 }
